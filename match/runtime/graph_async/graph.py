ddc1b0407e886a3d24228854cebe6465d7d49e4d
Haimrich <enrico.russo@phd.unict.it>
2025-06-26 14:59:26 +0200

    move MatchGraphRuntimeNodeCall in another file

--- match/runtime/graph_async/graph.py
+++ match/runtime/graph_async/graph.py
@@ -1,50 +1,16 @@
 from pathlib import Path
-from typing import Any, Dict, Optional, List
+from typing import Any, Dict, Optional
 
 import numpy as np
 import tvm
 
-from match.runtime.graph_async.memplan import MatchMemoryPlanner
-from match.runtime.graph_async.tensor import MatchMemoryTensor
 from match.target.target import MatchTarget
-from match.node.node import MatchNode
-from match.schedule.schedule import MatchSchedule
 from match.utils.utils import get_fname_node_schedule
 
+from .node import MatchGraphRuntimeNodeCall
+from .tensor import MatchMemoryTensor
+from .memplan import MatchMemoryPlanner
 
-
-class MatchGraphRuntimeNodeCall:
-    """
-    This class represent a call to a node (function) in the MATCH graph runtime.
-    """
-    def __init__(
-        self,
-        inputs: Optional[List[MatchMemoryTensor]] = None,
-        outputs: Optional[List[MatchMemoryTensor]] = None,
-        fn_name: str = "default_lib_1",
-        name: str = "default_lib_1",
-        node_info: dict = {},
-        node_id: int = 0,
-        node_name: Optional[str] = None,
-        schedule: Optional[MatchSchedule] = None,
-        match_node: Optional[MatchNode] = None,
-        num_parents: int = 0,
-        children: List[int] = None
-    ):
-        self.inputs = inputs
-        self.outputs = outputs
-        self.fn_name = fn_name
-        self.name = name
-        self.node_info = node_info
-        self.fallback = "match" not in self.fn_name
-        self.node_id = node_id
-        self.node_name = node_name
-        self.schedule = schedule
-        self.match_node = match_node
-        self.num_parents = num_parents
-        self.children = children or []
-        self.device_id = 0 if schedule is None else (schedule.exec_module.id + 1)
-        
 
 class MatchTVMGraphRuntime:
     """
---
+++ match/runtime/graph_async/node.py
@@ -0,0 +1,40 @@
+from typing import Optional, List
+
+from match.node.node import MatchNode
+from match.schedule.schedule import MatchSchedule
+
+from .tensor import MatchMemoryTensor
+
+
+class MatchGraphRuntimeNodeCall:
+    """
+    This class represent a call to a node (function) in the MATCH graph runtime.
+    """
+    def __init__(
+        self,
+        inputs: Optional[List[MatchMemoryTensor]] = None,
+        outputs: Optional[List[MatchMemoryTensor]] = None,
+        fn_name: str = "default_lib_1",
+        name: str = "default_lib_1",
+        node_info: dict = {},
+        node_id: int = 0,
+        node_name: Optional[str] = None,
+        schedule: Optional[MatchSchedule] = None,
+        match_node: Optional[MatchNode] = None,
+        num_parents: int = 0,
+        children: List[int] = None
+    ):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.fn_name = fn_name
+        self.name = name
+        self.node_info = node_info
+        self.fallback = "match" not in self.fn_name
+        self.node_id = node_id
+        self.node_name = node_name
+        self.schedule = schedule
+        self.match_node = match_node
+        self.num_parents = num_parents
+        self.children = children or []
+        self.device_id = 0 if schedule is None else (schedule.exec_module.id + 1)
+        
